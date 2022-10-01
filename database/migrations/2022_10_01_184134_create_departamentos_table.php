880a34d66b8fb717fa5739e78728bc4a0097383e
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-10-01 19:36:15 -0300

    Seeder departamento

--- database/migrations/2022_10_01_184134_create_departamentos_table.php
+++ database/migrations/2022_10_01_184134_create_departamentos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('departamentos', function (Blueprint $table) {
             $table->id();
-            $table->unsignedSmallInteger('codigo');
+            $table->string('codigo');
             $table->text('nombre');
             $table->timestamps();
         });
---
+++ database/seeders/DepartamentoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DepartamentoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('drivers')->insert([
+            'codigo'=>'DCIC',
+            'nombre'=>'Departamento de Ciencias e Ingeniería de la Computación'
+        ]);
+    }
+}
