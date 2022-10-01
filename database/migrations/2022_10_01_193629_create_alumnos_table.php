0767f097f32915e8c9286b945d5e7e5ae346cb56
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-10-01 16:58:02 -0300

    primera parte de BD

---
+++ database/migrations/2022_10_01_193629_create_alumnos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->smallInteger('lu')->unique();
+            $table->smallInteger('dni')->unique();
+            $table->date('fecha de nacimiento');
+            $table->string('mail')->unique();
+            $table->string('usuario')->unique();
+            $table->string('contraseña');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
