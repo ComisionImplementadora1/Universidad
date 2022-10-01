c8619d82c720003c87491b1a61ed25306b82bd4b
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-10-01 17:01:23 -0300

    migraciones

---
+++ database/migrations/2022_10_01_194033_create_examen_final_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examen_final', function (Blueprint $table) {
+            $table->id();
+            $table->text('observaciones');
+            $table->date('fecha');
+            $table->date('fecha_inicio_inscripciones');
+            $table->date('fecha_fin_inscripciones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examen_finals');
+    }
+};
---
+++ database/migrations/2022_10_01_194940_create_cursado_nota_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursado_nota', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('nota');
+            $table->unsignedSmallInteger('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursado_notas');
+    }
+};
