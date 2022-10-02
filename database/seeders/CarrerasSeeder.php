3a97647b4c6e23acbb50acd310c14bc483511112
Mathis MAZOYER <78586669+MathisMAZOYER@users.noreply.github.com>
2022-10-02 16:55:55 -0300

    Correccion driver -> carreras

--- database/seeders/CarrerasSeeder.php
+++ database/seeders/CarrerasSeeder.php
@@ -14,19 +14,19 @@ class CarrerasSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('drivers')->insert([
+        DB::table('carreras')->insert([
             'codigo'=>'LCC',
             'Nombre'=> 'Ingeniería en Sistemas de Información',
             'Materias_codigos' => '5912-5793-5551-7713-7791'
             ]);
 
-        DB::table('drivers')->insert([
+        DB::table('carreras')->insert([
             'codigo'=>'ISI',
             'Nombre'=> 'Ingeniería en Sistemas de Información',
             'Materias_codigos' => '5912-5793-5551-7713-7791-7714'
             ]);
 
-        DB::table('drivers')->insert([
+        DB::table('carreras')->insert([
             'codigo'=>'IC',
             'Nombre'=> 'Ingeniería en Computación',
             'Materias_codigos' => '5912-5793-5551-7713-7791-3051'
