2d994e5ab482b8187f324a990633c3f25c268f95
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-10-04 17:00:47 -0300

    secure assets35

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,13 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Universidad') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="{{ secure_asset('https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap')}}">
 
         <!-- Scripts -->
-        @vite([secure_asset('resources/css/app.css'), secure_asset('resources/js/app.js')])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
