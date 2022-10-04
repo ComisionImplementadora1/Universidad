8e0b4e03112cef0cd034a18c851ba455bcddd137
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-10-04 16:53:30 -0300

    secure assets3

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="{{ secure_asset('https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap')}}">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite([secure_asset('resources/css/app.css'), secure_asset('resources/js/app.js')])
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
