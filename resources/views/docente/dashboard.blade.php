6893592280b73ce38064649366c871053c040093
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-10-19 10:30:07 -0300

    usuario docente vistas y rutas

--- resources/views/docente/dashboard.blade.php
+++ resources/views/docente/dashboard.blade.php
@@ -1,17 +1,19 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html data-theme="autumn">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in as Docente!
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/slate/bootstrap.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>Universidad</title>
+</head>
+
+<body>
+    @include('docente.layouts.navigation')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
  
-Route::resource('materias', 'App\Http\Controllers\materiasController')->middleware(['auth:administrador', 'auth:alumno']);
-Route::resource('carreras', 'App\Http\Controllers\carrerasController')->middleware(['auth:administrador', 'auth:alumno']);
-Route::resource('departamentos', 'App\Http\Controllers\departamentosController')->middleware(['auth:administrador', 'auth:alumno']);
+Route::resource('materias', 'App\Http\Controllers\materiasController')->middleware(['auth:administrador', 'auth:alumno', 'auth:docente']);
+Route::resource('carreras', 'App\Http\Controllers\carrerasController')->middleware(['auth:administrador', 'auth:alumno', 'auth:docente']);
+Route::resource('departamentos', 'App\Http\Controllers\departamentosController')->middleware(['auth:administrador', 'auth:alumno', 'auth:docente']);
 Route::resource('alumnos', 'App\Http\Controllers\alumnosController')->middleware(['auth:administrador']);
 Route::resource('docentes', 'App\Http\Controllers\docentesController')->middleware(['auth:administrador']);
 
